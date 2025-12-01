dac3f93192f4162dbba9881d0064b1c1e6db80b8
AlexandreNoguez <alexandrenoguez_rs@yahoo.com.br>
2025-11-30 21:24:17 -0300

    fix(home-page): show new version

--- src/App.tsx
+++ src/App.tsx
@@ -21,7 +21,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>Código editado por Alexandre Noguez na release 1.0.6!</p>
+        <p>Código editado por Alexandre Noguez na release 1.0.8!</p>
         <p>Linha nova adicionada.</p>
         <p>Linha nova adicionada.</p>
         <p>Linha nova adicionada.</p>
